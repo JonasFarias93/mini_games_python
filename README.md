c1c5af01fb0acb83c7b58d50e01685c4220e73f8
JonasFarias93 <166467757+JonasFarias93@users.noreply.github.com>
2025-02-09 17:49:15 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# mini_games_python
+ Um repositório onde vou armarzenar meus minis games
---
+++ palavra_secreta/palavra_secreta.py
@@ -0,0 +1,37 @@
+import random
+
+biblioteca_secretas = ["banana", "computador", "oceano", "energia", "aventura",
+    "coração", "mistério", "relâmpago", "biblioteca", "viagem",
+    "universo", "silêncio", "maratona", "felicidade", "esmeralda",
+    "harmonia", "foguete", "enigma", "girassol", "labirinto"]
+
+palavra_secreta = random.choice(biblioteca_secretas)
+letra_acertada = ''
+numero_tentativas = 0
+
+while True:
+    letra_digitada = input('Digite uma letra: ')
+    numero_tentativas += 1
+    if len(letra_digitada) > 1:
+        print('Digite apenas uma letra')
+        continue
+
+    if letra_digitada in palavra_secreta:
+        letra_acertada += letra_digitada
+
+    palavra_formada = ''
+    for letra_secreta in palavra_secreta:
+        if letra_secreta in letra_acertada:
+            palavra_formada += letra_secreta
+        else:
+            palavra_formada += '*'           
+            
+    print(palavra_formada) 
+
+    if palavra_formada == palavra_secreta:
+        print('PARABÉNS VOCÊ GANHOU O JOGO!!')
+        print(f'A palavra secreta era {palavra_secreta}!!')
+        print(f'Você precisou de {numero_tentativas}')
+        palavra_secreta = random.choice(biblioteca_secretas)
+        letra_acertada = ''
+        numero_tentativas = 0
